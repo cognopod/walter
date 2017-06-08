c981ec76a8c05c35fc266f9855f9c76bef5859ac
Karl Bailey <karl@cognopod.com>
2017-06-08 13:05:02 -0400

    Update imports and added riverbend documentation

--- R/riverbend.R
+++ R/riverbend.R
@@ -1,3 +1,13 @@
+#' riverbend
+#'
+#' A function for automatically generating factor analysis riverplots wiht a sequence of solutions.
+#'
+#' @param data Data frame containing only variables to test.
+#' @param maxlatent Maximum number of latent variables to test.
+#' @param minlatent Minimum number of latent variables to test.
+#' @param cutoff Cutoff value for leaving a variable out of that factor analysis.
+#' @return Draws a riverplot
+#' @export
 riverbend <- function(data, maxlatent = NA, minlatent = NA, cutoff = 0.4) {
   FACTORS <- watershed(data, maxlatent = maxlatent, minlatent = minlatent, cutoff = cutoff)
   RIVER <- riverbank(dplyr::select(FACTORS, -ITEM))
