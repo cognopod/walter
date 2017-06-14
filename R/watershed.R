57480d1a08c9cda4da6c9cac916fd1c81e1501c4
Karl Bailey <karl@cognopod.com>
2017-06-14 00:05:37 -0400

    fix error in watershed

--- R/watershed.R
+++ R/watershed.R
@@ -8,11 +8,11 @@
 #' @param cutoff Cutoff value for leaving a variable out of that factor analysis.
 #' @return A data frame with maximum factor loading identified for each variable.
 
-watershed <- function(data, maxlatent = NA, minlatent = NA, cutoff = 0.4) {
+watershed <- function(data, maxlatent = ncol(data), minlatent = 1, cutoff = 0.4) {
   FACTORTEST <- data.frame(matrix(rep(NA,(maxlatent-(minlatent-1)) * (ncol(data))), ncol = maxlatent-(minlatent-1), nrow = ncol(data)))
   ITEM = c(LETTERS, c(t(outer(LETTERS, LETTERS, paste, sep = ""))))[1:ncol(data)]
   FACTORTEST <- cbind(ITEM, FACTORTEST)
-  for (i in (TEST$nlatent+2):1) {
+  for (i in (data$nlatent+2):1) {
     data.pa.oblimin <- psych::fa(data, nfactors=i, SMC=TRUE, fm="pa", rotate="oblimin", max.iter=100)
     FACTORTEST[,i+1] <- as.vector(apply(data.pa.oblimin$Structure,1,function(x) {if(max(abs(x)) >= cutoff) paste0(LETTERS[i], which.max(abs(x))) else NA}))
   }
